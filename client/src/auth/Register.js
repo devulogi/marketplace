f61ea5e65ffe259fb5cc5dfdd3ad09c8d56e8fc7
devulogi <devulogijrp.github@gmail.com>
2021-09-03 02:30:47 +0800

    Refactored: added form

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,7 +1,77 @@
+import { useState } from 'react';
+
+function RegistrationForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    alert(`
+      Name: ${name}
+      Email: ${email}
+      Password: ${password}
+    `);
+  };
+
+  return (
+    <form className='mt-3' onSubmit={handleFormSubmit}>
+      <div className='form-group mb-3'>
+        <label className='form-label'>Your Name</label>
+        <input
+          type='text'
+          value={name}
+          required
+          autoFocus
+          placeholder='Enter name'
+          className='form-control'
+          onChange={e => setName(e.target.value)}
+        />
+      </div>
+      <div className='form-group mb-3'>
+        <label className='form-label'>Your Email</label>
+        <input
+          type='email'
+          value={email}
+          required
+          placeholder='Enter Email'
+          className='form-control'
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group mb-3'>
+        <label className='form-label'>Your Password</label>
+        <input
+          type='password'
+          value={password}
+          required
+          minLength={6}
+          placeholder='Enter Password'
+          className='form-control'
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <button type='submit' className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+}
+
 export default function Register() {
   return (
-    <div className='container header p-5 text-center'>
-      <h1>Register Page!</h1>
-    </div>
+    <>
+      <div className='container-fluid bg-secondary p-5 text-center'>
+        <h1>Register</h1>
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-5 offset-md-3'>
+            <RegistrationForm />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
