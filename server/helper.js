d3128c339b1b35db071c8719d8cbe76c52cc45bd
devulogi <devulogijrp.github@gmail.com>
2021-09-03 07:28:49 +0800

    created: resource not found handler

--- server/helper.js
+++ server/helper.js
@@ -1,3 +1,7 @@
 export function errorHandler(err, req, res, next) {
   res.status(500).json({ ...err });
 }
+
+export function notFoundHandler(req, res) {
+  res.status(404).json({ error: 'resource not found!' });
+}
